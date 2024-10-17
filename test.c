86795e6b960ecbacda5877a69331d43d00c955fd
Kazuki Yoshida <yosshikazuki@gmail.com>
2024-10-18 00:44:50 +0900

    test

---
+++ test.c
@@ -0,0 +1,15 @@
+#include "/opt/homebrew/Cellar/check/0.15.2/include/check.h"
+#include <stdlib.h>
+#include <string.h>
+#include <utility.h>
+
+START_TEST()
+{
+
+}
+END_TEST
+
+int main(void)
+{
+	return 0;
+}
